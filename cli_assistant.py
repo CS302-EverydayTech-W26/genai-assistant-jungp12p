9b0748f5e2ce91f5bbd94cc4ce34427d65b065ca
jungp12p <jungp@carleton.edu>
2026-01-31 16:07:43 -0600

    GenAI deliverable

--- cli_assistant.py
+++ cli_assistant.py
@@ -1,7 +1,16 @@
 from gemini_client import *
 
 def main():
-    pass
+  client = GeminiClient()
+  
+  while True: 
+    user_input = input("Talk to me!:")
+    if user_input == "exit":
+      print("Goodbye!")
+      return
+    
+    response = client.generate_response(user_input)
+    print("chatbot response: ", response)
 
 if __name__ == "__main__":
   main()
